8b6cb3814d4f9b954adf6b7c26b1db92997dc830
Mikael Sundberg <misu02@me.com>
2019-03-02 18:01:12 +0100

    Rewrite

--- lib/functions.py
+++ lib/functions.py
@@ -18,3 +18,164 @@ def get_highest_location_id():
     return max(
         [int(x.get('id')) for x in get_locations("/Users/micke/PycharmProjects/skidresultat/static/skid_db.json")]
     )
+
+
+def get_seasons(c, skier):
+
+    c.execute("select distinct(a.name) from season a, results b, skier c where c.name='" + skier + "' and "
+              "b.skierid=c.id and a.id in (select distinct(seasonid) from results)")
+
+    return [x[0] for x in c.fetchall()][::-1]
+
+
+def get_biathlon_results(c, skier, season):
+
+    c.execute("select a.name, b.shooting, b.distance, b.time, b.result, b.competitionpart, b.shooting "
+              "from competition a, biathlon_results b, season c, skier d where "
+              "a.id=b.compid and b.seasonid=c.id and b.skierid=d.id and d.name='" + skier + "' and "
+              "c.name='" + season + "'")
+
+    res = c.fetchall()
+
+    c.execute(
+        "select count(a.result) from biathlon_results a, skier b, season c where b.name='" + skier + "' and "
+        "a.skierid=b.id and a.result == 1 and c.id=a.seasonid and c.name='" + season + "' group by a.result")
+    vinster = c.fetchall()
+    if len(vinster) == 0:
+        vinster = 0
+    else:
+        vinster = vinster[0][0]
+
+    c.execute(
+        "select count(a.result) from biathlon_results a, skier b, season c where b.name='" + skier + "' and "
+        "a.skierid=b.id and a.result == 2 and c.id=a.seasonid and c.name='" + season + "' group by a.result")
+    andra = c.fetchall()
+    if len(andra) == 0:
+        andra = 0
+    else:
+        andra = andra[0][0]
+
+    c.execute(
+        "select count(a.result) from biathlon_results a, skier b, season c where b.name='" + skier + "' and "
+        "a.skierid=b.id and a.result == 3 and c.id=a.seasonid and c.name='" + season + "' group by a.result")
+    tredje = c.fetchall()
+    if len(tredje) == 0:
+        tredje = 0
+    else:
+        tredje = tredje[0][0]
+
+    c.execute(
+        "select count(a.result) from biathlon_results a, skier b, season c where b.name='" + skier + "' and "
+        "a.skierid=b.id and c.id=a.seasonid and c.name='" + season + "'")
+    tot = c.fetchall()
+    c.execute(
+        "select count(a.result) from biathlon_results a, skier b, season c where b.name='" + skier + "' and "
+        "c.name='" + season + "' and c.id=a.seasonid and a.skierid=b.id and a.result < 4 and a.result > 0")
+    scountpallplatser = c.fetchall()
+
+    results = [x for x in res]
+
+    return results, vinster, andra, tredje, tot, scountpallplatser
+
+
+def get_results(c, skier, season):
+
+    c.execute("select a.name, a.tour, b.teqnique, b.distance, b.time, b.result, b.competitionpart, "
+              "b.tourresult from competition a, results b, season c, skier d where "
+              "a.id=b.compid and b.seasonid=c.id and b.skierid=d.id and d.name='" + skier + "' and "
+              "c.name='" + season + "' order by b.compid, b.competitionpart")
+
+    res = c.fetchall()
+
+    c.execute(
+        "select count(a.tourresult) from results a, skier b, season c where b.name='" + skier + "' and "
+        "a.skierid=b.id and a.competitionpart in (0, 1) and a.tourresult == 1 and c.id=a.seasonid and c.name='" +
+        season + "' group by a.tourresult")
+    vinster = c.fetchall()
+    if len(vinster) == 0:
+        vinster = 0
+    else:
+        vinster = vinster[0][0]
+
+    c.execute(
+        "select count(a.tourresult) from results a, skier b, season c where b.name='" + skier + "' and "
+        "a.skierid=b.id and a.competitionpart in (0, 1) and a.tourresult == 2 and c.id=a.seasonid and c.name='"
+        + season + "' group by a.tourresult")
+    andra = c.fetchall()
+    if len(andra) == 0:
+        andra = 0
+    else:
+        andra = andra[0][0]
+
+    c.execute(
+        "select count(a.tourresult) from results a, skier b, season c where b.name='" + skier + "' and "
+        "a.skierid=b.id and a.competitionpart in (0, 1) and a.tourresult == 3 and c.id=a.seasonid and c.name='" +
+        season + "' group by a.tourresult")
+    tredje = c.fetchall()
+    if len(tredje) == 0:
+        tredje = 0
+    else:
+        tredje = tredje[0][0]
+
+    c.execute(
+        "select count(a.tourresult) from results a, skier b, season c where b.name='" + skier + "' and "
+        "a.skierid=b.id and a.competitionpart in (0, 1) and c.id=a.seasonid and c.name='" + season + "'")
+    tot = c.fetchall()
+
+    c.execute(
+        "select count(a.tourresult) from results a, skier b, season c where b.name='Frida Bergqvist' and "
+        "c.name='" + season + "' and a.competitionpart in (0, 1) and c.id=a.seasonid and a.skierid=b.id "
+                              "and a.tourresult < 4 and a.tourresult > 0")
+    scountpallplatser = c.fetchall()
+
+    results = [x for x in res]
+
+    return results, vinster, andra, tredje, tot, scountpallplatser
+
+
+def get_pallplatser_index(c, skier):
+
+    c.execute(
+        "select count(a.tourresult) from results a, skier b where b.name='" + skier + "' and a.skierid=b.id and "
+        "a.tourresult < 4 and a.tourresult > 0  and a.competitionpart in (0, 1) group by a.tourresult")
+    pallplatser = c.fetchall()
+
+    c.execute(
+        "select count(a.tourresult) from results a, skier b where b.name='" + skier + "' and a.skierid=b.id")
+    tot = c.fetchall()
+
+    c.execute(
+        "select count(a.tourresult) from results a, skier b where b.name='" + skier + "' and a.skierid=b.id "
+        "and a.competitionpart in (0, 1)")
+    totcomp = c.fetchall()
+
+    c.execute(
+        "select count(a.tourresult) from results a, skier b where b.name='" + skier + "' and a.skierid=b.id and "
+        "a.tourresult < 4 and a.tourresult > 0 and a.competitionpart in (0, 1)")
+    countpallplatser = c.fetchall()
+
+    return pallplatser, tot, totcomp, countpallplatser
+
+
+def get_pallplatser_biathlon_index(c, skier):
+
+    c.execute(
+        "select count(a.result) from biathlon_results a, skier b where b.name='" + skier + "' and a.skierid=b.id and "
+        "a.result < 4 and a.result > 0 group by a.result")
+    pallplatser = c.fetchall()
+
+    c.execute(
+        "select count(a.result) from biathlon_results a, skier b where b.name='" + skier + "' and a.skierid=b.id")
+    tot = c.fetchall()
+
+    c.execute(
+        "select count(a.result) from biathlon_results a, skier b where b.name='" + skier + "' and a.skierid=b.id "
+        "and a.competitionpart in (0, 1)")
+    totcomp = c.fetchall()
+
+    c.execute(
+        "select count(a.result) from biathlon_results a, skier b where b.name='" + skier + "' and a.skierid=b.id and "
+        "a.result < 4 and a.result > 0")
+    countpallplatser = c.fetchall()
+
+    return pallplatser, tot, totcomp, countpallplatser
--- skidresultat-app.py
+++ skidresultat-app.py
@@ -2,7 +2,7 @@ import json
 import operator
 import os
 import sqlite3
-
+from lib.functions import *
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 def load_database(database):
     global conn
-    conn = sqlite3.connect(":memory:")
+    conn = sqlite3.connect(":memory:", check_same_thread=False)
     c = conn.cursor()
 
     try:
@@ -56,202 +56,51 @@ def load_database(database):
     return c
 
 
+def skidresults(skier, template, biathlon=False):
+
+    season_results = {}
+    season_pallplatser = {}
+    season_pallplatser_grouped = {}
+
+    seasons = get_seasons(dbconn, skier)
+    for season in seasons:
+
+        if biathlon:
+            results, fvinster, fandra, ftredje, stot, scountpallplatser = get_biathlon_results(dbconn, skier, season)
+        else:
+            results, fvinster, fandra, ftredje, stot, scountpallplatser = get_results(dbconn, skier, season)
+
+        results.sort(key=operator.itemgetter(0, 6))
+        season_results[season] = results
+        season_pallplatser[season] = [scountpallplatser[0], stot[0]]
+        season_pallplatser_grouped[season] = [fvinster, fandra, ftredje]
+
+    return render_template(template, seasons=seasons, season_results=season_results,
+                           season_pallplatser=season_pallplatser, season_pallplatser_grouped=season_pallplatser_grouped,
+                           namn=skier)
+
+
 @app.route('/frida')
 def frida_results():
 
-    season_results = {}
-    season_pallplatser = {}
-    season_pallplatser_grouped = {}
-
-    for season in fseasons:
-        dbconn.execute("select a.name, a.tour, b.teqnique, b.distance, b.time, b.result, b.competitionpart, "
-                       "b.tourresult from competition a, results b, season c, skier d where "
-                       "a.id=b.compid and b.seasonid=c.id and b.skierid=d.id and d.name='Frida Bergqvist' and "
-                       "c.name='" + season + "' order by b.compid, b.competitionpart")
-
-        res = dbconn.fetchall()
-
-        dbconn.execute(
-            "select count(a.tourresult) from results a, skier b, season c where b.name='Frida Bergqvist' and "
-            "a.skierid=b.id and a.competitionpart in (0, 1) and a.tourresult == 1 and c.id=a.seasonid and c.name='" +
-            season + "' group by a.tourresult")
-        vinster = dbconn.fetchall()
-        if len(vinster) == 0:
-            fvinster = 0
-        else:
-            fvinster = vinster[0][0]
-
-        dbconn.execute(
-            "select count(a.tourresult) from results a, skier b, season c where b.name='Frida Bergqvist' and "
-            "a.skierid=b.id and a.competitionpart in (0, 1) and a.tourresult == 2 and c.id=a.seasonid and c.name='"
-            + season + "' group by a.tourresult")
-        andra = dbconn.fetchall()
-        if len(andra) == 0:
-            fandra = 0
-        else:
-            fandra = andra[0][0]
-
-        dbconn.execute(
-            "select count(a.tourresult) from results a, skier b, season c where b.name='Frida Bergqvist' and "
-            "a.skierid=b.id and a.competitionpart in (0, 1) and a.tourresult == 3 and c.id=a.seasonid and c.name='" +
-            season + "' group by a.tourresult")
-        tredje = dbconn.fetchall()
-        if len(tredje) == 0:
-            ftredje = 0
-        else:
-            ftredje = tredje[0][0]
-
-        dbconn.execute(
-            "select count(a.tourresult) from results a, skier b, season c where b.name='Frida Bergqvist' and "
-            "a.skierid=b.id and a.competitionpart in (0, 1) and c.id=a.seasonid and c.name='" + season + "'")
-        stot = dbconn.fetchall()
-        dbconn.execute(
-            "select count(a.tourresult) from results a, skier b, season c where b.name='Frida Bergqvist' and "
-            "c.name='" + season + "' and a.competitionpart in (0, 1) and c.id=a.seasonid and a.skierid=b.id "
-                                  "and a.tourresult < 4 and a.tourresult > 0")
-        scountpallplatser = dbconn.fetchall()
-        results = [x for x in res]
-
-        results.sort(key=operator.itemgetter(0, 6))
-        print(results)
-        season_results[season] = results
-        season_pallplatser[season] = [scountpallplatser[0], stot[0]]
-        season_pallplatser_grouped[season] = [fvinster, fandra, ftredje]
-
-    return render_template('frida.html', seasons=fseasons, season_results=season_results,
-                           season_pallplatser=season_pallplatser, season_pallplatser_grouped=season_pallplatser_grouped)
-
-
-@app.route('/frida_biathlon')
-def frida_biathlon_results():
-
-    season_results = {}
-    season_pallplatser = {}
-    season_pallplatser_grouped = {}
-
-    for season in fseasons:
-        dbconn.execute("select a.name, b.shooting, b.distance, b.time, b.result, b.competitionpart, b.shooting "
-                       "from competition a, biathlon_results b, season c, skier d where "
-                       "a.id=b.compid and b.seasonid=c.id and b.skierid=d.id and d.name='Frida Bergqvist' and "
-                       "c.name='" + season + "'")
-
-        res = dbconn.fetchall()
-
-        dbconn.execute(
-            "select count(a.result) from biathlon_results a, skier b, season c where b.name='Frida Bergqvist' and "
-            "a.skierid=b.id and a.result == 1 and c.id=a.seasonid and c.name='" + season + "' group by a.result")
-        vinster = dbconn.fetchall()
-        if len(vinster) == 0:
-            fvinster = 0
-        else:
-            fvinster = vinster[0][0]
-
-        dbconn.execute(
-            "select count(a.result) from biathlon_results a, skier b, season c where b.name='Frida Bergqvist' and "
-            "a.skierid=b.id and a.result == 2 and c.id=a.seasonid and c.name='" + season + "' group by a.result")
-        andra = dbconn.fetchall()
-        if len(andra) == 0:
-            fandra = 0
-        else:
-            fandra = andra[0][0]
-
-        dbconn.execute(
-            "select count(a.result) from biathlon_results a, skier b, season c where b.name='Frida Bergqvist' and "
-            "a.skierid=b.id and a.result == 3 and c.id=a.seasonid and c.name='" + season + "' group by a.result")
-        tredje = dbconn.fetchall()
-        if len(tredje) == 0:
-            ftredje = 0
-        else:
-            ftredje = tredje[0][0]
-
-        dbconn.execute(
-            "select count(a.result) from biathlon_results a, skier b, season c where b.name='Frida Bergqvist' and "
-            "a.skierid=b.id and c.id=a.seasonid and c.name='" + season + "'")
-        stot = dbconn.fetchall()
-        dbconn.execute(
-            "select count(a.result) from biathlon_results a, skier b, season c where b.name='Frida Bergqvist' and "
-            "c.name='" + season + "' and c.id=a.seasonid and a.skierid=b.id and a.result < 4 and a.result > 0")
-        scountpallplatser = dbconn.fetchall()
-        results = [x for x in res]
-        results.sort(key=operator.itemgetter(0, 5))
-        season_results[season] = results
-        season_pallplatser[season] = [scountpallplatser[0], stot[0]]
-        season_pallplatser_grouped[season] = [fvinster, fandra, ftredje]
-
-    return render_template('frida_biathlon.html', seasons=fseasons, season_results=season_results,
-                           season_pallplatser=season_pallplatser, season_pallplatser_grouped=season_pallplatser_grouped)
+    return skidresults('Frida Bergqvist', 'frida.html')
 
 
 @app.route('/tilde')
 def tilde_results():
 
-    season_results = {}
-    season_pallplatser = {}
-    season_pallplatser_grouped = {}
+    return skidresults('Tilde Bergqvist', 'tilde.html')
 
-    for season in fseasons:
-        dbconn.execute("select a.name, a.tour, b.teqnique, b.distance, b.time, b.result, b.competitionpart, "
-                       "b.tourresult from competition a, results b, season c, skier d where "
-                       "a.id=b.compid and b.seasonid=c.id and b.skierid=d.id and d.name='Tilde Bergqvist' and "
-                       "c.name='" + season + "' order by b.compid, b.competitionpart")
 
-        res = dbconn.fetchall()
+@app.route('/frida_biathlon')
+def frida_biathlon_results():
 
-        dbconn.execute(
-            "select count(a.tourresult) from results a, skier b, season c where b.name='Tilde Bergqvist' and "
-            "a.skierid=b.id and a.competitionpart in (0, 1) and a.tourresult == 1 and c.id=a.seasonid and "
-            "c.name='" + season + "' group by a.tourresult")
-        vinster = dbconn.fetchall()
-        if len(vinster) == 0:
-            fvinster = 0
-        else:
-            fvinster = vinster[0][0]
-
-        dbconn.execute(
-            "select count(a.tourresult) from results a, skier b, season c where b.name='Tilde Bergqvist' and "
-            "a.skierid=b.id and a.competitionpart in (0, 1) and a.tourresult == 2 and c.id=a.seasonid and "
-            "c.name='" + season + "' group by a.tourresult")
-        andra = dbconn.fetchall()
-        if len(andra) == 0:
-            fandra = 0
-        else:
-            fandra = andra[0][0]
-
-        dbconn.execute(
-            "select count(a.tourresult) from results a, skier b, season c where b.name='Tilde Bergqvist' and "
-            "a.skierid=b.id and a.competitionpart in (0, 1) and a.tourresult == 3 and c.id=a.seasonid and "
-            "c.name='" + season + "' group by a.result")
-        tredje = dbconn.fetchall()
-        if len(tredje) == 0:
-            ftredje = 0
-        else:
-            ftredje = tredje[0][0]
-
-        dbconn.execute(
-            "select count(a.tourresult) from results a, skier b, season c where b.name='Tilde Bergqvist' and "
-            "a.skierid=b.id and a.competitionpart in (0, 1) and c.id=a.seasonid and c.name='" + season + "'")
-        stot = dbconn.fetchall()
-        dbconn.execute(
-            "select count(a.tourresult) from results a, skier b, season c where b.name='Tilde Bergqvist' and "
-            "c.name='" + season + "' and a.competitionpart in (0, 1) and c.id=a.seasonid and a.skierid=b.id and "
-                                  "a.tourresult < 4 and a.tourresult > 0")
-        scountpallplatser = dbconn.fetchall()
-        results = [x for x in res]
-        results.sort(key=operator.itemgetter(0, 6))
-        print(results)
-        season_results[season] = results
-        season_pallplatser[season] = [scountpallplatser[0], stot[0]]
-        season_pallplatser_grouped[season] = [fvinster, fandra, ftredje]
-
-    return render_template('tilde.html', seasons=tseasons, season_results=season_results,
-                           season_pallplatser=season_pallplatser, season_pallplatser_grouped=season_pallplatser_grouped)
+    return skidresults('Frida Bergqvist', 'frida_biathlon.html', True)
 
 
 @app.route('/')
 def index():
 
-    global fseasons
-    global tseasons
     global dbconn
 
     dbconn = load_database('static/skid_db.json')
@@ -260,65 +109,11 @@ def index():
     with sqlite3.connect('/tmp/backup.db') as new_db:
         new_db.executescript("".join(conn.iterdump()))
 
-    dbconn.execute("select distinct(a.name) from season a, results b, skier c where c.name='Frida Bergqvist' and "
-                   "b.skierid=c.id and a.id in (select distinct(seasonid) from results)")
-    fseasons = [x[0] for x in dbconn.fetchall()][::-1]
+    fpallplatser, ftot, ftotcomp, fcountpallplatser = get_pallplatser_index(dbconn, 'Frida Bergqvist')
 
-    dbconn.execute("select distinct(a.name) from season a, results b, skier c where c.name='Tilde Bergqvist' and "
-                   "b.skierid=c.id and a.id in (select distinct(seasonid) from results)")
-    tseasons = [x[0] for x in dbconn.fetchall()][::-1]
+    b_fpallplatser, b_ftot, b_ftotcomp, b_fcountpallplatser = get_pallplatser_biathlon_index(dbconn, 'Frida Bergqvist')
 
-    dbconn.execute(
-        "select count(a.tourresult) from results a, skier b where b.name='Frida Bergqvist' and a.skierid=b.id and "
-        "a.tourresult < 4 and a.tourresult > 0  and a.competitionpart in (0, 1) group by a.tourresult")
-    fpallplatser = dbconn.fetchall()
-
-    dbconn.execute(
-        "select count(a.tourresult) from results a, skier b where b.name='Frida Bergqvist' and a.skierid=b.id")
-    ftot = dbconn.fetchall()
-
-    dbconn.execute(
-        "select count(a.tourresult) from results a, skier b where b.name='Frida Bergqvist' and a.skierid=b.id "
-        "and a.competitionpart in (0, 1)")
-    ftotcomp = dbconn.fetchall()
-
-    dbconn.execute(
-        "select count(a.tourresult) from results a, skier b where b.name='Frida Bergqvist' and a.skierid=b.id and "
-        "a.tourresult < 4 and a.tourresult > 0 and a.competitionpart in (0, 1)")
-    fcountpallplatser = dbconn.fetchall()
-
-    dbconn.execute(
-        "select count(a.result) from biathlon_results a, skier b where b.name='Frida Bergqvist' and a.skierid=b.id and "
-        "a.result < 4 and a.result > 0 group by a.result")
-    b_fpallplatser = dbconn.fetchall()
-
-    dbconn.execute(
-        "select count(a.result) from biathlon_results a, skier b where b.name='Frida Bergqvist' and a.skierid=b.id")
-    b_ftot = dbconn.fetchall()
-
-    dbconn.execute(
-        "select count(a.result) from biathlon_results a, skier b where b.name='Frida Bergqvist' and a.skierid=b.id and "
-        "a.result < 4 and a.result > 0")
-    b_fcountpallplatser = dbconn.fetchall()
-
-    dbconn.execute(
-        "select count(a.tourresult) from results a, skier b where b.name='Tilde Bergqvist' and a.skierid=b.id and "
-        "a.result < 4 and a.result > 0 and a.competitionpart in (0, 1) group by a.tourresult")
-    tpallplatser = dbconn.fetchall()
-
-    dbconn.execute(
-        "select count(a.tourresult) from results a, skier b where b.name='Tilde Bergqvist' and a.skierid=b.id")
-    ttot = dbconn.fetchall()
-
-    dbconn.execute(
-        "select count(a.tourresult) from results a, skier b where b.name='Tilde Bergqvist' and a.skierid=b.id "
-        "and a.competitionpart in (0, 1)")
-    ttotcomp = dbconn.fetchall()
-
-    dbconn.execute(
-        "select count(a.tourresult) from results a, skier b where b.name='Tilde Bergqvist' and a.skierid=b.id and "
-        "a.tourresult < 4 and a.tourresult > 0 and a.competitionpart in (0, 1)")
-    tcountpallplatser = dbconn.fetchall()
+    tpallplatser, ttot, ttotcomp, tcountpallplatser = get_pallplatser_index(dbconn, 'Tilde Bergqvist')
 
     return render_template('index.html', fpallplatser=fpallplatser, ftot=ftot, fcountpallplatser=fcountpallplatser,
                            tpallplatser=tpallplatser, ttot=ttot, tcountpallplatser=tcountpallplatser,
