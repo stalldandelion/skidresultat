47b3f584788240cda00cca25e3193d9c36ad7bcd
Mikael Sundberg <misu02@me.com>
2021-03-15 19:39:10 +0100

    Updated

--- skidresultat-app.py
+++ skidresultat-app.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env skidr_python
+#!/usr/bin/env python
 import operator
 import os
-import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
 import sqlite3
 from lib.functions import *
 from flask import Flask, render_template
@@ -17,45 +15,112 @@ def load_database(database):
     c = conn.cursor()
 
     try:
-        c.execute('CREATE TABLE competition (id int, name text, tour int)')
-        c.execute('CREATE TABLE season (id int, name text)')
-        c.execute('CREATE TABLE skier (id int, name text)')
-        c.execute('CREATE TABLE results (id int, compid int, seasonid int, skierid int, competitionpart int,'
-                  ' result int, distance text, teqnique text, time text, tourresult int, tour int)')
-        c.execute('CREATE TABLE biathlon_results (id int, compid int, seasonid int, skierid int, competitionpart int,'
-                  ' result int, distance text, teqnique text, time text, shooting text, tour int)')
-        c.execute('CREATE UNIQUE INDEX result_index ON results(id, compid, seasonid, skierid, competitionpart)')
-        c.execute('CREATE UNIQUE INDEX biathlon_result_index ON biathlon_results(id, compid, seasonid, skierid)')
+        c.execute("CREATE TABLE competition (id int, name text, tour int)")
+        c.execute("CREATE TABLE season (id int, name text)")
+        c.execute("CREATE TABLE skier (id int, name text)")
+        c.execute(
+            "CREATE TABLE results (id int, compid int, seasonid int, skierid int, competitionpart int,"
+            " result int, distance text, teqnique text, time text, tourresult int, tour int)"
+        )
+        c.execute(
+            "CREATE TABLE biathlon_results (id int, compid int, seasonid int, skierid int, competitionpart int,"
+            " result int, distance text, teqnique text, time text, shooting text, tour int)"
+        )
+        c.execute(
+            "CREATE UNIQUE INDEX result_index ON results(id, compid, seasonid, skierid, competitionpart)"
+        )
+        c.execute(
+            "CREATE UNIQUE INDEX biathlon_result_index ON biathlon_results(id, compid, seasonid, skierid)"
+        )
     except sqlite3.OperationalError as e:
         print(e)
 
     json_data = dict()
-    for jsonfile in glob.glob(database + '/*.json'):
+    for jsonfile in glob.glob(database + "/*.json"):
         json_data.update(json.load(open(jsonfile)))
 
-    for sqlstatement in json_data['competition']:
-        c.execute("insert into competition values (" + sqlstatement['id'] + ",'" + sqlstatement['name'] +
-                  "','" + sqlstatement['tour'] + "')")
+    for sqlstatement in json_data["competition"]:
+        c.execute(
+            "insert into competition values ("
+            + sqlstatement["id"]
+            + ",'"
+            + sqlstatement["name"]
+            + "','"
+            + sqlstatement["tour"]
+            + "')"
+        )
 
-    for sqlstatement in json_data['season']:
-        c.execute("insert into season values (" + sqlstatement['id'] + ",'" + sqlstatement['name'] + "')")
+    for sqlstatement in json_data["season"]:
+        c.execute(
+            "insert into season values ("
+            + sqlstatement["id"]
+            + ",'"
+            + sqlstatement["name"]
+            + "')"
+        )
 
-    for sqlstatement in json_data['skier']:
-        c.execute("insert into skier values (" + sqlstatement['id'] + ",'" + sqlstatement['name'] + "')")
+    for sqlstatement in json_data["skier"]:
+        c.execute(
+            "insert into skier values ("
+            + sqlstatement["id"]
+            + ",'"
+            + sqlstatement["name"]
+            + "')"
+        )
 
-    for sqlstatement in json_data['results']:
-        if sqlstatement['teqnique'] == 'Biathlon':
-            c.execute("insert into biathlon_results values (" + sqlstatement['id'] + "," + sqlstatement['compid'] +
-                      ", " + sqlstatement['seasonid'] + ", " + sqlstatement['skierid'] + ", " +
-                      sqlstatement['competitionpart'] + ", " + sqlstatement['result'] + ", '" +
-                      sqlstatement['distance'] + "', '" + sqlstatement['teqnique'] + "', '" + sqlstatement['time'] +
-                      "', '" + sqlstatement['shooting'] + "', '" + sqlstatement['tour'] + "')")
+    for sqlstatement in json_data["results"]:
+        if sqlstatement["teqnique"] == "Biathlon":
+            c.execute(
+                "insert into biathlon_results values ("
+                + sqlstatement["id"]
+                + ","
+                + sqlstatement["compid"]
+                + ", "
+                + sqlstatement["seasonid"]
+                + ", "
+                + sqlstatement["skierid"]
+                + ", "
+                + sqlstatement["competitionpart"]
+                + ", "
+                + sqlstatement["result"]
+                + ", '"
+                + sqlstatement["distance"]
+                + "', '"
+                + sqlstatement["teqnique"]
+                + "', '"
+                + sqlstatement["time"]
+                + "', '"
+                + sqlstatement["shooting"]
+                + "', '"
+                + sqlstatement["tour"]
+                + "')"
+            )
         else:
-            c.execute("insert into results values (" + sqlstatement['id'] + "," + sqlstatement['compid'] +
-                      ", " + sqlstatement['seasonid'] + ", " + sqlstatement['skierid'] + ", " +
-                      sqlstatement['competitionpart'] + ", " + sqlstatement['result'] + ", '" +
-                      sqlstatement['distance'] + "', '" + sqlstatement['teqnique'] + "', '" + sqlstatement['time'] +
-                      "', '" + sqlstatement['tourresult'] + "', '" + sqlstatement['tour'] + "')")
+            c.execute(
+                "insert into results values ("
+                + sqlstatement["id"]
+                + ","
+                + sqlstatement["compid"]
+                + ", "
+                + sqlstatement["seasonid"]
+                + ", "
+                + sqlstatement["skierid"]
+                + ", "
+                + sqlstatement["competitionpart"]
+                + ", "
+                + sqlstatement["result"]
+                + ", '"
+                + sqlstatement["distance"]
+                + "', '"
+                + sqlstatement["teqnique"]
+                + "', '"
+                + sqlstatement["time"]
+                + "', '"
+                + sqlstatement["tourresult"]
+                + "', '"
+                + sqlstatement["tour"]
+                + "')"
+            )
 
     conn.commit()
 
@@ -73,9 +138,18 @@ def skidresults(skier, template, biathlon=False):
     for season in seasons:
 
         if biathlon:
-            results, fvinster, fandra, ftredje, stot, scountpallplatser = get_biathlon_results(dbconn, skier, season)
+            (
+                results,
+                fvinster,
+                fandra,
+                ftredje,
+                stot,
+                scountpallplatser,
+            ) = get_biathlon_results(dbconn, skier, season)
         else:
-            results, fvinster, fandra, ftredje, stot, scountpallplatser = get_results(dbconn, skier, season)
+            results, fvinster, fandra, ftredje, stot, scountpallplatser = get_results(
+                dbconn, skier, season
+            )
 
         results.sort(key=operator.itemgetter(0, 6))
         season_results[season] = results
@@ -83,54 +157,76 @@ def skidresults(skier, template, biathlon=False):
         season_pallplatser_grouped[season] = [fvinster, fandra, ftredje]
 
     print(seasons)
-    return render_template(template, seasons=seasons, season_results=season_results,
-                           season_pallplatser=season_pallplatser, season_pallplatser_grouped=season_pallplatser_grouped,
-                           namn=skier)
+    return render_template(
+        template,
+        seasons=seasons,
+        season_results=season_results,
+        season_pallplatser=season_pallplatser,
+        season_pallplatser_grouped=season_pallplatser_grouped,
+        namn=skier,
+    )
 
 
-@app.route('/frida')
+@app.route("/frida")
 def frida_results():
 
-    return skidresults('Frida Bergqvist', 'frida.html')
+    return skidresults("Frida Bergqvist", "frida.html")
 
 
-@app.route('/tilde')
+@app.route("/tilde")
 def tilde_results():
 
-    return skidresults('Tilde Bergqvist', 'tilde.html')
+    return skidresults("Tilde Bergqvist", "tilde.html")
 
 
-@app.route('/frida_biathlon')
+@app.route("/frida_biathlon")
 def frida_biathlon_results():
 
-    return skidresults('Frida Bergqvist', 'frida_biathlon.html', True)
+    return skidresults("Frida Bergqvist", "frida_biathlon.html", True)
 
 
-@app.route('/')
+@app.route("/")
 def index():
 
     global dbconn
 
-    dbconn = load_database('static/database')
-    if os.path.isfile('/tmp/backup.db'):
-        os.remove('/tmp/backup.db')
-    with sqlite3.connect('/tmp/backup.db') as new_db:
+    dbconn = load_database("static/database")
+    if os.path.isfile("/tmp/backup.db"):
+        os.remove("/tmp/backup.db")
+    with sqlite3.connect("/tmp/backup.db") as new_db:
         new_db.executescript("".join(conn.iterdump()))
 
-    fpallplatser, ftot, ftotcomp, fcountpallplatser = get_pallplatser_index(dbconn, 'Frida Bergqvist')
+    fpallplatser, ftot, ftotcomp, fcountpallplatser = get_pallplatser_index(
+        dbconn, "Frida Bergqvist"
+    )
 
-    b_fpallplatser, b_ftot, b_ftotcomp, b_fcountpallplatser = get_pallplatser_biathlon_index(dbconn, 'Frida Bergqvist')
+    (
+        b_fpallplatser,
+        b_ftot,
+        b_ftotcomp,
+        b_fcountpallplatser,
+    ) = get_pallplatser_biathlon_index(dbconn, "Frida Bergqvist")
 
-    tpallplatser, ttot, ttotcomp, tcountpallplatser = get_pallplatser_index(dbconn, 'Tilde Bergqvist')
+    tpallplatser, ttot, ttotcomp, tcountpallplatser = get_pallplatser_index(
+        dbconn, "Tilde Bergqvist"
+    )
 
-    return render_template('index.html', fpallplatser=fpallplatser, ftot=ftot, fcountpallplatser=fcountpallplatser,
-                           tpallplatser=tpallplatser, ttot=ttot, tcountpallplatser=tcountpallplatser,
-                           b_fpallplatser=b_fpallplatser, b_ftot=b_ftot, b_fcountpallplatser=b_fcountpallplatser,
-                           ftotcomp=ftotcomp, ttotcomp=ttotcomp)
+    return render_template(
+        "index.html",
+        fpallplatser=fpallplatser,
+        ftot=ftot,
+        fcountpallplatser=fcountpallplatser,
+        tpallplatser=tpallplatser,
+        ttot=ttot,
+        tcountpallplatser=tcountpallplatser,
+        b_fpallplatser=b_fpallplatser,
+        b_ftot=b_ftot,
+        b_fcountpallplatser=b_fcountpallplatser,
+        ftotcomp=ftotcomp,
+        ttotcomp=ttotcomp,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    app.run(host='localhost',
-            port=8080,
-            debug=True)
+    app.run(host="localhost", port=8080, debug=True)
